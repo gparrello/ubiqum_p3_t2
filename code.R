2fa234438f5efc3fd658220e1af8b507f356adcc
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-02-01 10:35:20 +0100

    add train set, create aggregation function, set correct start for ts

--- code.R
+++ code.R
@@ -6,11 +6,12 @@ pacman::p_load(
   "imputeTS",
   "rbokeh",
   "padr",
-  "forecast"
+  "forecast",
+  "ggplot2"
 )
 set.seed(123)
 
-queryfunction <- function(){
+fQuery <- function(){
   years <- c('2006', '2007', '2008', '2009', '2010')
   conn = dbConnect(
     MySQL(),
@@ -34,53 +35,8 @@ queryfunction <- function(){
   return(df)
 }
 
-original_file <- "./data/original.csv"
-processed_file <- "./data/processed.csv"
-if(file.exists(processed_file)){
-  print("loading processed data from file")
-  # df <- read.csv(processed_file)
-  df <- fread(processed_file)
-  df$Datetime <- as.POSIXct(df$Datetime, origin="1970-01-01", tz="GMT")
-} else {
-  if(!file.exists(original_file)){
-    print("querying original data")
-    df <- queryfunction()
-    print("writing original data to file")
-    # write.csv(df, file=original_file)
-    fwrite(df, file = original_file, dateTimeAs = "epoch")
-  } else {
-    print("loading original data from file")
-    # df <- read.csv(original_file)
-    df <- fread(original_file)
-  }
-
-  # some transformations
-  df$Datetime <- paste(df$Date, '', df$Time)
-  df$Datetime <- as.POSIXct(df$Datetime, "%Y/%m/%d %H:%M:%S", tz="GMT")
-  # attr(df$Datetime, "tzone") <- "GMT" # better than "Europe/Paris"
-  df[,c("Date","Time","id")] <- NULL
-  # original_df <- df
-  df <- pad(df, break_above = 3e6)
-  df <- na.kalman(df)
-  # try interpolation, kalman, spline and random
-
-  print("writing processed data to file")
-  # write.csv(df, file=processed_file)
-  fwrite(df, file = processed_file, dateTimeAs = "epoch")
-
-}
-
-print("calculating granularity")
-# attr(df$Datetime, "tzone") <- "GMT" # better than "Europe/Paris"
-aggregated_df <- c()
-aggregated_ts <- c()
-decomposed_ts <- c()
-# decomposed_ts2 <- c()
-granularity <- c("hour", "day", "week", "month")
-frequency <- c(365.25*24, 365, 52, 12)
-names(frequency) <- granularity
-for(g in granularity){
-  aggregated_df[[g]] <- df %>%
+fAggregate <- function(input){
+  output <- input %>%
     group_by(date = cut(Datetime, g)) %>%  # hour grouping not working!
     summarize(
       obs = n(),
@@ -106,25 +62,90 @@ for(g in granularity){
       voltage_max = mean(Voltage) + sd(Voltage)
     )
   
-  aggregated_df[[g]]$obs <- as.integer(aggregated_df[[g]]$obs)
+  output$obs <- as.integer(output$obs)
+  
+  return(output)
+}
+
+
+
+original_file <- "./data/original.csv"
+processed_file <- "./data/processed.csv"
+if(file.exists(processed_file)){
+  print("loading processed data from file")
+  # df <- read.csv(processed_file)
+  df <- fread(processed_file)
+  df$Datetime <- as.POSIXct(df$Datetime, origin="1970-01-01", tz="GMT")
+} else {
+  if(!file.exists(original_file)){
+    print("querying original data")
+    df <- fQuery()
+    print("writing original data to file")
+    # write.csv(df, file=original_file)
+    fwrite(df, file = original_file, dateTimeAs = "epoch")
+  } else {
+    print("loading original data from file")
+    # df <- read.csv(original_file)
+    df <- fread(original_file)
+  }
+
+  # some transformations
+  df$Datetime <- paste(df$Date, '', df$Time)
+  df$Datetime <- as.POSIXct(df$Datetime, "%Y/%m/%d %H:%M:%S", tz="GMT")
+  # attr(df$Datetime, "tzone") <- "GMT" # better than "Europe/Paris"
+  df[,c("Date","Time","id")] <- NULL
+  # original_df <- df
+  df <- pad(df, break_above = 3e6)
+  df <- na.kalman(df)
+  # try interpolation, kalman, spline and random
+
+  print("writing processed data to file")
+  # write.csv(df, file=processed_file)
+  fwrite(df, file = processed_file, dateTimeAs = "epoch")
+
+}
+
+
+print("calculating granularity")
+
+aggregated_df <- c()
+aggregated_ts <- c()
+decomposed_ts <- c()
+
+granularity <- c("hour", "day", "week", "month")
+frequency <- c(365.25*24, 365, 52, 12)
+start <- c(
+  1,
+  yday(df$Datetime[1]),  # calculate the day of the year for the first row
+  week(df$Datetime[1]),
+  month(df$Datetime[1])
+)
+names(frequency) <- granularity
+names(start) <- granularity
+for(g in granularity){
+  aggregated_df[[g]] <- fAggregate(df)
   
   aggregated_ts[[g]] <- ts(
     aggregated_df[[g]],
-    frequency = frequency[[g]]
+    frequency = frequency[[g]],
+    start=c(2006,start[[g]])
   )
   
   my_vector <- c()
-  my_vector2 <- c()
   for(c in colnames(aggregated_ts[[g]])[!colnames(aggregated_ts[[g]]) %in% c("date", "obs")]){
     my_vector[[c]] <- stl(
       aggregated_ts[[g]][,c],
       s.window = "periodic"
+      # s.window = frequency[[g]]
     )
-    # my_vector2[[c]] <- stl(
-    #   aggregated_ts[[g]][,c],
-    #   s.window = frequency[[g]]
-    # )
+    
   }
   decomposed_ts[[g]] <- my_vector
-  # decomposed_ts2[[g]] <- my_vector2
+  
 }
+
+
+## Forecasting
+
+train_month <- window(aggregated_ts[["month"]][,"active_sum"], start=c(2006,12), end=c(2009,12))
+# test_month <- 
